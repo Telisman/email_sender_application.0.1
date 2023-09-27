cfc2993e074227091b399878d9e1e0cb244e13fc
Davor Telisman <davortelisman@gmail.com>
2023-09-27 13:31:31 +0200

    checking the Excel file to see if it meets one of the specified conditions

--- home.py
+++ home.py
@@ -18,10 +18,28 @@ def upload_file():
         return "No selected file"
 
     if file:
-        df = pd.read_excel(file)
+        # Assuming the file is in Excel format (xlsx)
+        try:
+            df = pd.read_excel(file)
+        except Exception as e:
+            return f"Error reading the file: {str(e)}"
+
+        file_columns = df.columns.tolist()
+
+        required_columns = ['email', 'subject', 'first name', 'last name']
+
+        missing_columns = [col for col in required_columns if col not in file_columns]
+
+        if missing_columns:
+            missing_msg = f"The following required columns are missing: {', '.join(missing_columns)}"
+        else:
+            missing_msg = "All required columns are present in the Excel file."
+
+        present_columns_msg = f"Columns present in the file: {', '.join(file_columns)}"
+
         print(df.head())
 
-        return "File uploaded and processed successfully"
+        return f"{present_columns_msg}<br>{missing_msg}"
 
 if __name__ == '__main__':
     app.run()
